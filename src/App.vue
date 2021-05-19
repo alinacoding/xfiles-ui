3fcda247a4a59d830e1f4aa5a67eaefb07e0c8a0
Alina <a.voicu24@gmail.com>
2021-05-19 23:50:56 +0300

    Added VueJS starter files and chat component

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="app">
+		<ChatContainer/>
+	</div>
+</template>
+
+<script>
+
+import ChatContainer from './components/ChatContainer.vue'
+
+export default {
+	name: 'App',
+		components: {
+			ChatContainer
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
