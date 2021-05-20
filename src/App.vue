e73efa42ba37af2a2bc8e8ec44333403ddc74710
Alina <a.voicu24@gmail.com>
2021-05-20 19:12:41 +0300

    Added test users to firebase

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 	<div id="app">
 		<ChatContainer/>
 		<button @click="printUsersData">Print Data</button>
+		<button @click="addUsersData" :disabled="updatingData">Add Data</button>
 	</div>
 </template>
 
@@ -16,9 +17,44 @@ export default {
 	components: {
 		ChatContainer
 	}, 
+	data () {
+		return {
+			users: [
+				{
+					_id: '6R0MijpK6M4AIrwaaCY2',
+					username: 'Luke',
+					avatar: 'https://66.media.tumblr.com/avatar_c6a8eae4303e_512.pnj'
+				},
+				{
+					_id: 'SGmFnBZB4xxMv9V4CVlW',
+					username: 'Leia',
+					avatar: 'https://avatarfiles.alphacoders.com/184/thumb-184913.jpg'
+				},
+				{
+					_id: '6jMsIXUrBHBj7o2cRlau',
+					username: 'Yoda',
+					avatar:
+						'https://vignette.wikia.nocookie.net/teamavatarone/images/4/45/Yoda.jpg/revision/latest?cb=20130224160049'
+				}
+			], 
+			updatingData: false
+		}
+	},
 	methods : {
+		async addUsersData() {
+			this.updatingData = true;
+			const user1 = this.users[0];
+			await usersRef.doc(user1._id).set(user1)
+			const user2 = this.users[1];
+			await usersRef.doc(user2._id).set(user2)
+			const user3 = this.users[2];
+			await usersRef.doc(user3._id).set(user3)
+			this.updatingData = false;
+		
+		},
 		printUsersData() {
-			usersRef.get().then(querySnapshot => querySnapshot.forEach(doc => {
+			usersRef.get()
+			.then(querySnapshot => querySnapshot.forEach(doc => {
 					console.log(doc.data())
 			})).catch(err => {
 				console.log('Error getting documents', err);
