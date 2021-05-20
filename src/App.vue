e8d82e674a8b5870dc3778265d3c52a08f260c52
Alina <a.voicu24@gmail.com>
2021-05-20 15:26:49 +0300

    added firebase config key

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
 	<div id="app">
 		<ChatContainer/>
+		<button @click="printUsersData">Print Data</button>
 	</div>
 </template>
 
 <script>
 
 import ChatContainer from './components/ChatContainer.vue'
+import { usersRef } from '@/firestore'
+
 
 export default {
 	name: 'App',
-		components: {
-			ChatContainer
+	components: {
+		ChatContainer
+	}, 
+	methods : {
+		printUsersData() {
+			usersRef.get().then(querySnapshot => querySnapshot.forEach(doc => {
+					console.log(doc.data())
+			})).catch(err => {
+				console.log('Error getting documents', err);
+			});
+		}
 	}
 }
 </script>
---
+++ src/firestore/index.js
@@ -0,0 +1,21 @@
+import app from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/database'
+import 'firebase/storage'
+
+const config = {
+	apiKey: "AIzaSyCJIkxx9FZGz8VkqNAFK8_cWbcfhoE6G7g",
+	authDomain: "xfiles-ui.firebaseapp.com",
+	projectId: "xfiles-ui",
+	storageBucket: "xfiles-ui.appspot.com",
+	messagingSenderId: "425418248200",
+	appId: "1:425418248200:web:3f074646f1b225c49e01bd",
+	measurementId: "G-BYFSMGQL85"
+}
+
+app.initializeApp(config)
+
+export const firebase = app
+export const db = app.firestore()
+
+export const usersRef = db.collection('users')
